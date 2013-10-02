e185e66a202b1bc618d5cb9b8a34e1d9f850333c
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-10-02 19:16:20 +0530

    controller names should be pluralized - fixed path issue

--- bin/duct.js
+++ bin/duct.js
@@ -2,7 +2,9 @@
 
 var util = require('util'),
   fs = require('fs'),
-  path = require('path');
+  _ = require('lodash'),
+  path = require('path'),
+  pluralize = require('../lib/pluralize');
 
 /**
   * Check if Duct is loaded
@@ -26,8 +28,31 @@ if (!Duct) {
   */
 var args = process.argv,
     cmd = args[2],
+    nameARGV = args[3],
     root = process.cwd(),
     templatesPath = '../templates';
+
+/**
+  * this function would give you the main template for the controller which is dynamically formed
+  * meaning, if you the command says :
+  *  duct generate controller post
+  * 
+  * then the file should be posts_controller.js
+  */
+function controllerName( name ) {
+  return controllerName = formattedControllerName( name ) + 'Controller';
+};
+
+/**
+  * Give the controller name in a proper format i.e. Capitalized and Pluralized
+  */
+function formattedControllerName( name ) {
+  return _.capitalize( pluralize.inflect( name ) );
+};
+
+function controllerDestination ( name ) {
+  root + '/app/controllers' + pluralize.inflect( name ) + '_controller.js';
+}
 
 process.nextTick(function () {
   
@@ -38,11 +63,12 @@ process.nextTick(function () {
   switch (val) {
     case 'controller':
       console.log('Generating a controller from the command line');
+
       var controller_template = fs.readFileSync( templatesPath + '/controller_template.js', 'utf8', function(err, data){
         if(err) throw err;
       });
 
-      fs.fs.writeFileSync( root + '/app/controllers', 'controller_template', 'utf8', function(err) {
+      fs.fs.writeFileSync( controllerDestination( nameARGV ) , controllerName( nameARGV ), 'utf8', function(err) {
         if (err) throw err;
         console.log('Controller created.');
       });
