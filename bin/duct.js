310cb8a412da8c2c4ddf88e9ca5d8b160060619d
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-10-05 01:25:39 +0530

    stage set for new duct app generator

--- bin/duct.js
+++ bin/duct.js
@@ -13,30 +13,50 @@
   *  todo: pull it out into a different module
   */
 var args = process.argv,
-    cmd = args[3],
+    gen = args[2],
+    cmd = appName = args[3],
     nameARGV = args[4],
-    ControllerGenerator = require('../lib/generators/controller');
+    ControllerGenerator = require('../lib/generators/controller'),
+    newAppGenerator = require('../lib/generators/new');
 
 process.nextTick(function () {
   
   /**
     * trigger appropirate operations for generators 
     */
-  switch (cmd) {
-    case 'controller':
-      (new ControllerGenerator( nameARGV )).generate();
+  switch(gen){
+    case 'g':
+    case 'generate':
+      switch (cmd) {
+        case 'controller':
+          (new ControllerGenerator( nameARGV )).generate();
+          break;
+
+        case 'model':
+          console.log('Generating a model from the command line is coming soon');
+          break;
+
+        default:
+        case'h':
+        case 'help':
+          console.log('Usage for Duct');
+          console.log('command                        purpose');
+          console.log('g  generate controller <name>  generates a controller with routes');
+          break;
+      };
       break;
 
-    case 'model':
-      console.log('Generating a model from the command line is coming soon');
+    case 'new':
+      console.log('Generating new Duct app..');
+      (new newAppGenerator( appName )).generate();
       break;
 
-    default:
-    case'h':
-    case 'help':
-      console.log('Usage for Duct');
-      console.log('command                        purpose');
-      console.log('g  generate controller <name>  generates a controller with routes');
-      break;
-  }
+      default:
+      case'h':
+      case 'help':
+        console.log('Usage for Duct');
+        console.log('command                        purpose');
+        console.log('g  generate controller <name>  generates a controller with routes');
+        break;
+  };
 });
